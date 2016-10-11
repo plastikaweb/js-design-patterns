9bb90870407264c364684072a6ae33d7a235e984
Carlos Matheu Armengol <info@plastikaweb.com>
2016-10-11 16:59:38 +0200

    format code for revealing module 2

--- revealing-module-pattern/revealing-module-pattern-2.js
+++ revealing-module-pattern/revealing-module-pattern-2.js
@@ -1,23 +1,23 @@
 'use strict';
 
-var calculator = function(eq) {
+var calculator = function (eq) {
     //private
     var eqCtl = document.getElementById(eq),
-    add = function(x, y) {
-        var result = x + y;
-        eqCtl.innerHTML = result;
-        console.log(result);
-    },
-    substract = function(x, y) {
-        var result = x - y;
-        eqCtl.innerHTML = result;
-        console.log(result);
-    }
-    //public
+        add = function (x, y) {
+            var result = x + y;
+            eqCtl.innerHTML = result;
+            console.log(result);
+        },
+        substract = function (x, y) {
+            var result = x - y;
+            eqCtl.innerHTML = result;
+            console.log(result);
+        }
+        //public
     return {
         add: add,
         substract: substract
     };
 }('output');
 
-calculator.add(3,2);
+calculator.add(3, 2);
